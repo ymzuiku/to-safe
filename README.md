f99d4e5df2c644ae1ea48848b4752e11056b53b4
pillar <pillar.liang@bindo.com>
2018-07-12 11:48:43 +0800

    init

---
+++ README.md
@@ -0,0 +1,48 @@
+# Safe Obj
+
+## Install
+
+```
+$ npm install --save safe-obj
+```
+
+## painful in Javascript
+```
+var obj = {a:'bb'}
+var value;
+
+// if you get obj.a.b[2].c, is Error
+if(obj.a && obj.a.b && obj.a.b[2] && obj.a.b[2].c) {
+  value = obj.a.b[2].c;
+}
+
+```
+
+## Case1: Change SafeObj
+
+```
+import toSafe from 'to-safe'
+var obj = { a: 'bb'};
+obj = toSafe(obj);
+d = obj.a.b.c.d;  // {isNull: true}
+console.log(d.e[2].g[0].f);  // {isNull: true}
+
+```
+
+## Case2: Use function callbak
+
+```
+import toSafe from 'to-safe'
+var obj = { a: 'bb'};
+var value = toSafe(()=>obj.a.b.c.d[2].e);
+console.log(value);  // {isNull: true}
+```
+
+## Set isNull Key
+
+```
+import toSafe from 'to-safe'
+var obj = { a: 'bb'};
+obj = toSafe(obj, 'empty');
+console.log(obj.a.b.c.d);  // {empty: true}
+```
---
+++ src/index.js
@@ -0,0 +1,28 @@
+function toSafe(obj, undefinedKey = 'isNull', isUndefined) {
+  if (Object.prototype.toString.call(obj) === '[object Function]') {
+    try {
+      return obj();
+    } catch (error) {
+      return { [undefinedKey]: true };
+    }
+  }
+  if (isUndefined) {
+    obj = { [undefinedKey]: true };
+  }
+  return new Proxy(obj, {
+    get: function(target, key) {
+      var value = Reflect.get(target, key);
+      if (
+        value === undefined ||
+        (Object.prototype.toString.call(value) !== '[object Object]' &&
+          Object.prototype.toString.call(value) !== '[object Array]' &&
+          Object.prototype.toString.call(value) !== '[object Function]')
+      ) {
+        return toSafe(target, undefinedKey, true);
+      }
+      return value;
+    }
+  });
+}
+
+module.exports = toSafe;
